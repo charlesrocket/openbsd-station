40ed03511475f306dd932faeb6b2a10d29110473
-k <slowdive@me.com>
2022-02-18 02:57:04 -0800

    Update motd.sh

--- roles/common/files/motd.sh
+++ roles/common/files/motd.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
 figlet "$(hostname)" | lolcat -f && echo
+
+printf "$(sysctl -a | grep hw.model | sed 's/hw.model/CPU/')\n"
+printf "$(sysctl -a | grep hw.ncpu | sed 's/hw.ncpu/Cores/')\n"
+printf "$(sysctl -a | grep hw.physmem | sed 's/hw.physmem/Memory/')\n\n"
