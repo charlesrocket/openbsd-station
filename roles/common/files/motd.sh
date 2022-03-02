724f56b346783cbfa35926bbd54ea8706d8402bd
-k <slowdive@me.com>
2022-03-02 05:45:57 -0800

    Reformat `status`

--- roles/common/files/motd.sh
+++ roles/common/files/motd.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 figlet "$(hostname)" | lolcat -f && echo
 
-status="$(sysctl -a | grep -E 'machdep.bootmethod|hw.model|hw.ncpu|hw.realmem|vfs.zfs.version.module|hw.machine_arch|kern.securelevel' | sed -e 's/machdep.bootmethod/Boot method/' -e 's/hw.model/CPU/' -e 's/hw.ncpu/Cores/' -e 's/hw.realmem/Memory/' -e 's/vfs.zfs.version.module/ZFS version/' -e 's/hw.machine_arch/Architecture/' -e 's/kern.securelevel/Security level/')\n\n"
+status="$(sysctl -a | grep -E 'machdep.bootmethod|hw.model|hw.ncpu|hw.realmem|vfs.zfs.version.module|hw.machine_arch|kern.securelevel' | sed \
+-e 's/machdep.bootmethod/Boot method/' \
+-e 's/hw.model/CPU/' \
+-e 's/hw.ncpu/Cores/' \
+-e 's/hw.realmem/Memory/' \
+-e 's/vfs.zfs.version.module/ZFS version/' \
+-e 's/hw.machine_arch/Architecture/' \
+-e 's/kern.securelevel/Security level/')\n\n"
 
 printf "$status"
