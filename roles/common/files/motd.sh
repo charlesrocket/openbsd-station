7743205233f311a52c9e26801f2130b9ba614703
-k <slowdive@me.com>
2022-03-02 05:38:11 -0800

    Update motd.sh

--- roles/common/files/motd.sh
+++ roles/common/files/motd.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 figlet "$(hostname)" | lolcat -f && echo
 
-printf "$(sysctl -a | grep hw.model | sed 's/hw.model/CPU/')\n"
-printf "$(sysctl -a | grep hw.ncpu | sed 's/hw.ncpu/Cores/')\n"
-printf "$(sysctl -a | grep hw.realmem | sed 's/hw.realmem/Memory/')\n\n"
+status="$(sysctl -a | grep -E 'machdep.bootmethod|hw.model|hw.ncpu|hw.realmem|vfs.zfs.version.module|hw.machine_arch|kern.securelevel' | sed -e 's/machdep.bootmethod/Boot method/' -e 's/hw.model/CPU/' -e 's/hw.ncpu/Cores/' -e 's/hw.realmem/Memory/' -e 's/vfs.zfs.version.module/ZFS version/' -e 's/hw.machine_arch/Architecture/' -e 's/kern.securelevel/Security level/')\n\n"
+
+printf "$status"
