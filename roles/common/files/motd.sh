049d22fb18f210676f6c85b61892fe94668db22b
-k <slowdive@me.com>
2022-05-22 19:21:13 -0700

    Update motd

--- roles/common/files/motd.sh
+++ roles/common/files/motd.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 figlet "$(hostname)" | lolcat -f && echo
 
-status="$(sysctl -a | grep -E 'machdep.bootmethod|hw.model|hw.ncpu|hw.realmem|vfs.zfs.version.module|hw.machine_arch|kern.securelevel' | sed \
+status="$(sysctl -a | grep -E 'machdep.bootmethod|hw.model|hw.ncpu|hw.realmem|vfs.zfs.version.module|hw.machine_arch' | sed \
 -e 's/machdep.bootmethod/Boot method/' \
 -e 's/hw.model/CPU/' \
 -e 's/hw.ncpu/Cores/' \
 -e 's/hw.realmem/Memory/' \
 -e 's/vfs.zfs.version.module/ZFS version/' \
--e 's/hw.machine_arch/Architecture/' \
--e 's/kern.securelevel/Security level/')\n\n"
+-e 's/hw.machine_arch/Architecture/')\n\n"
 
 printf "$status"
